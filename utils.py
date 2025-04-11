e78ca2cc10ec53453884942d3f38e4b32febd31d
TitanSage02 <eayiwahoun@gmail.com>
2025-04-11 23:14:40 +0100

    Refactor email sending functionality and load environment variables for SMTP configuration

--- utils.py
+++ utils.py
@@ -1,3 +1,7 @@
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
 def formater_resultat(response_json):
     """
     Transforme la réponse JSON de SmartEye en un message formaté.
@@ -30,3 +34,42 @@ def formater_resultat(response_json):
         f"**Commentaire :** {commentaire}"
     )
     return message
+
+
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def send_mail(msg, destinataire):
+    # Informations d'authentification 
+    serveur_username = os.environ.get('SERVEUR_USERNAME')
+    serveur_password = os.environ.get('SERVEUR_PASSWORD')
+
+    # Informations sur l'email à envoyer
+    expediteur = 'SmartEye@gmail.com'
+    destinataire = destinataire
+    sujet = 'SmartEye - Alerte'
+    corps_message = f'{msg}'
+
+    # Configuration du serveur SMTP de SendinBlue
+    serveur_smtp = 'smtp-relay.sendinblue.com'
+    port_smtp = 587
+
+    # Création du message
+    message = MIMEMultipart()
+    message['From'] = expediteur
+    message['To'] = ', '.join(destinataire)
+    message['Subject'] = sujet
+    message.attach(MIMEText(corps_message, 'plain'))
+
+    # Connexion au serveur SMTP de SendinBlue
+    serveur = smtplib.SMTP(serveur_smtp, port_smtp)
+    serveur.starttls()
+    serveur.login(serveur_username, serveur_password)
+
+    # Envoi de l'email
+    serveur.sendmail(expediteur, destinataire, message.as_string())
+
+    # Fermeture de la connexion au serveur SMTP
+    serveur.quit()
